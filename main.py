f55ac91bf24e1e1450d6aaf9693deea5a706d864
Antoine Caillon <caillonantoine@gmail.com>
2017-10-30 17:02:10 +0100

    Chirp log

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def write(file,array):
 		
 def generate_chirp(f0,f1,T,fe):
 	space = np.linspace(0,T,fe*T)
-	return chirp(space,f0,T,f1)
+	return chirp(space,f0,T,f1,method='logarithmic')
 	
 def radix2(array):
 	N = len(array)
