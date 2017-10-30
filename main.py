342b1cd3ed1b670034fa2436f40b59e66a3175c4
Antoine Caillon <caillonantoine@gmail.com>
2017-10-30 13:28:51 +0100

    fix bug import

--- main.py
+++ main.py
@@ -2,14 +2,14 @@
 import numpy as np
 import wave as wa
 from scipy.io import wavfile as wf
-import scipy.signal.chirp as chirp
+from scipy.signal import chirp
 
 def read(file):
 	return wf.read(file)[1]
 	
 def write(file,array):
 	with wa.open(file,'w') as output:
-		array = ''.join(wa.struct.pack('h',elm) for elm in ((array*32767).astype('int'))
+		array = ''.join([wa.struct.pack('h',elm) for elm in ((array*32767).astype('int'))])
 		output.setparams((1,2,44100,0,'NONE','not compressed'))
 		output.writeframes(array)
 		
