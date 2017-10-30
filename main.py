127569c86445ed91fb88db8c8b77de149f0546ea
Antoine Caillon <caillonantoine@gmail.com>
2017-10-30 13:23:07 +0100

    Ajout de radix et chirp

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import numpy as np
 import wave as wa
 from scipy.io import wavfile as wf
+import scipy.signal.chirp as chirp
 
 def read(file):
 	return wf.read(file)[1]
@@ -11,3 +12,19 @@ def write(file,array):
 		array = ''.join(wa.struct.pack('h',elm) for elm in ((array*32767).astype('int'))
 		output.setparams((1,2,44100,0,'NONE','not compressed'))
 		output.writeframes(array)
+		
+def generate_chirp(f0,f1,T,fe):
+	space = np.linspace(0,T,fe*T)
+	return chirp(space,f0,T,f1)
+	
+def radix2(array):
+	N = len(array)
+	radix = 0
+	while 1:
+		if 2**radix < N:
+			radix += 1
+		else:
+			break
+	Y = np.zeros(radix)
+	Y[0:N] = array
+	return array
